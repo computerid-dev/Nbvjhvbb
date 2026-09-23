2c62335886b4a43db494907010efadddf7d94c73
computerid-dev <nugrohokelyn@gmail.com>
2026-09-23 08:49:14 +0700

    Upload anti-clone.js

---
+++ anti-clone.js
@@ -0,0 +1,10 @@
+/*
+ * anti-clone.js
+ * Nugrohos Music - Yulian Nugrohos Group
+ *
+ * File ini menandai bahwa produk ini terdaftar sebagai karya original
+ * milik Nugroho Y.R. Lihat README.md dan LICENSE untuk ketentuan
+ * penggunaan dan penjualan kembali.
+ */
+
+console.log('Halaman ini dilindungi anti-clone.js');
